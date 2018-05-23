7ddafaa99472e5b0ec34f6d99272dc255ee2ec28
Lucile Gentner <lucile.gentner@gmail.com>
2018-05-23 19:18:36 +0200

    Create API method getting all products from a specified category - without pagination

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * Class CategoryController
@@ -87,5 +88,62 @@ class CategoryController extends Controller
         $response->setContent($categoriesJSON);
 
         return $response;
+    }
+
+    /**
+     * Get list of products from a specified category
+     *
+     * @Route("/api/categories/{id}/products/", name="get_products_from_category")
+     * @Method({"GET"})
+     */
+    public function getProductsFromCategoryAction($id, Request $request)
+    {
+
+        //Get pagination params
+        $page = $request->query->get('page') !== null ? (int)$request->query->get('page') : 1;
+        $limit = $request->query->get('limit') !== null ? (int)$request->query->get('limit') : 10;
+
+        $category = $this->getDoctrine()
+            ->getRepository("AppBundle:Category")
+            ->find($id);
+
+        $allProducts = $this->getDoctrine()
+            ->getRepository("AppBundle:Product")
+            ->findBy(['category' => $category]);
+
+        $nbProducts = count($allProducts);
+
+        $dql = "SELECT p FROM AppBundle:Product p WHERE p.category = :category";
+        $query = $this->getDoctrine()->getEntityManager()->createQuery($dql)
+            ->setParameter('category', $category)
+            ->setFirstResult(($page-1)*$limit)
+            ->setMaxResults($limit);
+
+        $products = $query->getResult();
+
+        //Build array of results
+        $results = [];
+
+        foreach ($products as $product) {
+            $results[]= ['id' => $product->getId(),
+                'name' => $product->getName(),
+                'price' => $product->getPrice(),
+                'stock' => $product->getStock(),
+                'category' => $category->getName()];
+        }
+
+        //Encode in JSON
+        $response = new Response();
+        $productsJSON = json_encode(array(
+            'nbProducts' => $nbProducts,
+            'nbPages' => (int)($nbProducts/$limit+1),
+            'products' => $results
+                                            ));
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent($productsJSON);
+
+        return $response;
+
     }
 }
